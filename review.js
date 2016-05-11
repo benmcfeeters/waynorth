46be001c8468090d065e7e3277d80198e909066f
benmcfeeters <benmcfeeters@gmail.com>
2016-05-11 13:57:03 +0100

    Create review.js

---
+++ review.js
@@ -0,0 +1,11 @@
+
+var Innova = Innova || {};
+Innova.options = { key: 'xxlhbZ2cqJRgjuvDoDzYiA' };
+(function () {
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = 'https://s3.amazonaws.com/innovareviews/embed.js';
+var x = document.getElementsByTagName('script')[0];
+x.parentNode.insertBefore(s, x);
+})();
